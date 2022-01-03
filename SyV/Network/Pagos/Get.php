e5ab1e3bf2a98fdf41732d77d28f69ab6d0431e3
SebastianVillarreal <sebas_villalva13@hotmail.com>
2022-01-02 20:32:33 -0600

    Update Get.php

--- SyV/Network/Pagos/Get.php
+++ SyV/Network/Pagos/Get.php
@@ -26,32 +26,32 @@
   // $nombre = $_POST['nombre'];
   // $direccion = $_POST['direccion'];
 
-  $data = json_decode(file_get_contents("php://input"));
+  // $data = json_decode(file_get_contents("php://input"));
 
-  // set product property values
-  //$obra->nombre = $data->nombre;
-  //$obra->direccion = $data->direccion;
-  //$obra->id_usuario = $id_usuario;
+  // // set product property values
+  // //$obra->nombre = $data->nombre;
+  // //$obra->direccion = $data->direccion;
+  // //$obra->id_usuario = $id_usuario;
 
-  $stmt = $obra->read();
-  $num = $stmt->rowCount();
+  // $stmt = $obra->read();
+  // $num = $stmt->rowCount();
 
-  $obras_arr=array();
-  if($num > 0){
-    while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-      // extract row
-      // this will make $row['name'] to
-      // just $name only
-      extract($row);
-      $obra_item = array(
-        "Id" => $Id,
-        "Total" => $Total,
-        "Nombre" => $Nombre,
-        "Fecha" => $Fecha
-      );
-      array_push($obras_arr, $obra_item);
-    }
-    //echo json_encode($obras_arr);
-  }
-  echo json_encode($obras_arr);
+  // $obras_arr=array();
+  // if($num > 0){
+  //   while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+  //     // extract row
+  //     // this will make $row['name'] to
+  //     // just $name only
+  //     extract($row);
+  //     $obra_item = array(
+  //       "Id" => $Id,
+  //       "Total" => $Total,
+  //       "Nombre" => $Nombre,
+  //       "Fecha" => $Fecha
+  //     );
+  //     array_push($obras_arr, $obra_item);
+  //   }
+  //   //echo json_encode($obras_arr);
+  // }
+  // echo json_encode($obras_arr);
  ?>
